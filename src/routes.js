25c7a1ea96a6ee13f3853b6539bac0085cf9bb6b
Aslamdesusa <aslam17@navgurukul.org>
2018-12-30 02:31:02 +0530

    first commit

---
+++ src/routes.js
@@ -0,0 +1,71 @@
+// adding some npm modules
+let admin = require("firebase-admin");
+let firebase = require("firebase");
+let Joi = require("joi");
+
+// making routes
+const routes = [
+{
+	method: 'GET',
+	path: '/',
+	handler: async(request, h)=>{
+		return ('!Hello')
+	}
+},
+{
+	method: 'POST',
+	path: '/user/sign-up',
+	config: {
+	// include this route in swagger documentation
+		description:"Post user details",
+	    notes:"route not for publicaly use",
+		tags:['api'],
+			validate: {
+				// request payload
+				payload: {
+			    	firstname : Joi.string().required(),
+			    	lastname : Joi.string().required(),
+			    	username : Joi.string().required(),
+			    	email : Joi.string().required(),
+					password : Joi.string().required(),
+					mobile : Joi.number().required(),
+					gender : Joi.string().required(),
+					business: Joi.string().required(),
+					isVerified : false
+				}
+			}
+		},
+		handler: async(request, h)=>{
+		// making refrence with firebase as table name Users
+		let ref = firebase.database().ref('Users');
+		let pr = async (resolve, reject) => {
+			let newUser = ({
+				"firstname": request.payload.firstname,
+				"lastname": request.payload.lastname,
+				"username": request.payload.username,
+				"email": request.payload.email,
+				"password": request.payload.password,
+				"mobile": request.payload.mobile,
+				"gender": request.payload.gender,
+				"business": request.payload.business,
+				"isValid": false
+			});			
+			ref.push(newUser)
+			if (newUser.length == 0) {
+				return resolve({
+					statusCode: 401,
+					message: "Operation not successfully Completed there is something messing"
+				});
+			}else{
+				return resolve({
+					statusCode: 200,
+					message: "Operation successfully Completed",
+					data: newUser
+				});
+			}
+		}
+		return new Promise(pr)
+	} 
+},	
+]
+export default routes;
---
+++ src/server.js
@@ -0,0 +1,96 @@
+//import firebase from firebase
+import * as firebase from 'firebase';
+
+// import routes from routes
+import routes from './routes'
+
+// user hapi framwork on nodejs
+const Hapi = require('hapi');
+
+// require some npm modules
+const Inert = require('inert');
+const Vision = require('vision');
+const HapiSwagger = require('hapi-swagger');
+
+const url = "https://basehippo.firebaseio.com/"
+
+
+
+
+// validation function
+const validate = async function (user, decoded, request) {
+    // checks to see if the person is valid
+    if (!user['_id']) {
+        return { isValid: false };
+    }
+    else {
+        return { isValid: true };
+    }
+};
+
+
+const init = async () => {
+    //creating a server
+    const server = new Hapi.Server({
+        port: 8080,
+        routes: { cors: true }
+    });
+    const swaggerOptions = {
+        info: {
+            title: 'Test API Documentation'
+        },
+    };
+
+    //register hapi swagger documentation
+    await server.register([
+        Inert,
+        Vision,
+        {
+            plugin: HapiSwagger,
+            options: swaggerOptions
+        }
+    ]);
+
+    // cookie
+    await server.register(require('hapi-auth-cookie'));
+      server.auth.strategy('restricted', 'cookie',
+      {
+        ttl: 24 * 60 * 60 * 1000,
+        password: 'vZiYpmTzqXMp8PpYXKwqc9ShQ1UhyAfy',
+        cookie: 'basehippo',
+        isSecure: false,
+        redirectTo: '/',
+        isSameSite: 'Lax'
+      });
+
+    //register hapi-auth-jwt2 for authentication
+    await server.register(require('hapi-auth-jwt2'));
+
+    server.auth.strategy('jwt', 'jwt',
+        {
+            key: 'vZiIpmTzqXHp8PpYXTwqc9SRQ1UAyAfC',
+            validate: validate,
+            verifyOptions: { algorithms: ['HS256'] }
+        });
+
+    await firebase.initializeApp({
+      serviceAccount: "../basehippo-firebase-adminsdk-c3w73-fa9d80fef4.json",
+      databaseURL: url
+    });
+
+    //registering all routes
+    server.route(routes);
+
+    await server.start();
+    console.log(`Server running at: ${server.info.uri}`);
+
+    console.log(`Database connection succeeded from ${url}`)
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
